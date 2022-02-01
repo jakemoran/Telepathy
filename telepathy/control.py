3fcd11393d1ff045998be52fa2e1baa472d0191a
jakemoran <jakemoran44@yahoo.com>
2022-01-31 19:18:32 -0500

    Update control.py

--- telepathy/control.py
+++ telepathy/control.py
@@ -11,11 +11,31 @@ from pathlib import Path
 TIMEOUT = 60
 
 
-def deg2hr(deg: float):
+class Target(BaseModel):
+    name: Optional[str] = None
+    ra: float
+    dec: float
+    exposure_length: Optional[int] = None
+    num_exposures: Optional[int] = None
+
+
+def deg2hr(deg: float) -> float:
+    """Convert degree to hour angle for RA"""
     return (deg / 360) * 24
+
+def pointing_error(ra: float, dec: float, target: Target) -> tuple:
+    """Determine pointing offset for a given target"""
+    ra_error, dec_error = abs(target.ra - ra), abs(target.dec - dec)
+    return (ra_error, dec_error)
+
+def within_tolerance(error: tuple, tol: float) -> bool:
+    """Determine if pointing error is within tolerance"""
+    ra_error, dec_error = error
+    return (ra_error <= tol and dec_error <= tol)
 
 
 def status_check(func):
+    """Run mount checks before slewing/syncing (possibly unnecessary)"""
     def wrapper(*args, **kwargs):
         tel = args[0].telescope
         assert tel.CanPark
@@ -29,32 +49,40 @@ def status_check(func):
     return wrapper
 
 
-class Target(BaseModel):
-    name: Optional[str] = None
-    ra: float
-    dec: float
-    exposure_length: Optional[int] = None
-    num_exposures: Optional[int] = None
-
-
 class Session(BaseModel):
     apikey: str = Field(..., description="Authentication key for astrometry api")
     image_path: str = Field(..., description="Path to images")
     FOV_width: float = Field(..., description="Width in degrees of camera FOV")
     targets: List[Target] = Field(..., description="List of targets")
 
+    connect_camera: bool = False
+    connect_telescope: bool = False
+    connect_focuser: bool = False
+    connect_filter_wheel: bool = False
+
     camera: Any = None
     telescope: Any = None
+    focuser: Any = None
+    filter_wheel: Any = None
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.camera = self.connect_camera()
-        self.telescope = self.connect_telescope()
+        if self.connect_camera:
+            self.camera = self.camera_init()
+        if self.connect_telescope:
+            self.telescope = self.telescope_init()
 
 
-    def plate_solve(self, target: Target, image_name: str = "output.fits",
+    def plate_solve(self, target: Optional[Target] = None, image_name: str = "output.fits",
                     exp_time: float = 0.5, gain: int = 9, tol: float = 1 / 60, attempts: int = 5):
+
+        if target is None:
+            try:
+                target = self.targets[0]
+            except IndexError:
+                print("Target list empty, aborting...")
+                return
 
         if self.telescope.AtPark:
             self.telescope.Unpark()
@@ -78,19 +106,19 @@ class Session(BaseModel):
                 break
 
             pointing_ra, pointing_dec = solution["ra"], solution["dec"]
-            ra_error, dec_error = abs(target.ra - pointing_ra), abs(target.dec - pointing_dec)
+            error = pointing_error(pointing_ra, pointing_dec, target)
 
-            print(f"Pointing error - RA: {ra_error}, DEC: {dec_error}")
+            print(f"Pointing error - RA: {round(error[0], 4)}, DEC: {round(error[1], 4)}")
             print("Syncing...")
 
-            self.sync_telescope(ra=pointing_ra, dec=pointing_dec)
-            if ra_error <= tol and dec_error <= tol:
+            #self.sync_telescope(ra=pointing_ra, dec=pointing_dec)
+            if within_tolerance(error, tol):
                 print(f"Plate solve succeeded in {i + 1} attempt" + ("s" if i > 0 else ""))
                 break
             elif i == attempts - 1:
                 print("Attempt limit reached, aborting...")
 
-    def take_image(self, duration: float, gain: int, output: Union[Path, str]):
+    def take_image(self, duration: float, gain: int, output: Union[Path, str] = "output.fits"):
         if self.camera.Connected and self.camera.CameraState == 0:  # Camera state 0 implies camera is idle
             self.camera.Gain = gain
             self.camera.StartExposure(duration, True)
@@ -108,6 +136,10 @@ class Session(BaseModel):
             raise ConnectionError("Process failed: Camera unavailable")
 
     def save_image(self, output: Union[Path, str]):
+        if not self.camera.ImageReady:
+            print("No image to be saved")
+            return
+
         print(f"Saving image to {output}")
         img = self.camera.ImageArray
         newhdu = fits.PrimaryHDU(np.array(img))
@@ -130,7 +162,7 @@ class Session(BaseModel):
         self.telescope.SyncToCoordinates(ra, dec)
 
     @staticmethod
-    def connect_camera():
+    def camera_init():
         cam = win.Dispatch("ASCOM.DSLR.Camera")
         cam.Connected = True
         if cam.Connected:
@@ -140,7 +172,7 @@ class Session(BaseModel):
             raise ConnectionError("Camera failed to connect")
 
     @staticmethod
-    def connect_telescope():
+    def telescope_init():
         tel = win.Dispatch("EQMOD.Telescope")
         tel.Connected = True
         if tel.Connected:
